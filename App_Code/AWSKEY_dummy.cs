c53b71f680f585d1c3156e2a3688d2b83a47e791
take <dalmore.name@gmail.com>
2018-11-28 10:09:56 +0900

    AWSの秘密鍵 dummyクラスを追加

---
+++ App_Code/AWSKEY_dummy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// SecretAWSKeys AWSの秘密鍵
+/// </summary>
+public static class SecretAWSKeys_dummy
+{
+    //Please setting your AWS Keys
+    public const string ACCESS_KEY_ID = "********************";
+    public const string AWS_SECRET_KEY = "********************";
+    public const string ASSOCIATE_TAG = "********************";
+}
